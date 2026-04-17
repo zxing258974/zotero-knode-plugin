21f2869abf2e6c232da03782090e726812552c78
xing zhang <zhangx@youngerinfo.com>
2026-04-17 09:22:28 +0800

    支持 zotero 9.0 版本

--- esbuild.js
+++ esbuild.js
@@ -2,11 +2,87 @@ const path = require('path')
 const fs = require('fs')
 const esbuild = require('esbuild')
 const rmrf = require('rimraf')
+rmrf.sync('build')
 rmrf.sync('gen')
 
 require('zotero-plugin/copy-assets')
-require('zotero-plugin/rdf')
-require('zotero-plugin/version')
+require('zotero-plugin/make-manifest')
+
+function xmlEscape(value) {
+  return String(value)
+    .replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+}
+
+function generateLegacyMetadata() {
+  const pkg = require('./package.json')
+  const supported = JSON.parse(fs.readFileSync(path.join(__dirname, 'schema', 'supported.json'), 'utf8'))
+  const { version } = require('./gen/version.cjs')
+
+  const installRdf = `<?xml version="1.0" encoding="utf-8" ?>
+<RDF xmlns="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:em="http://www.mozilla.org/2004/em-rdf#">
+  <Description about="urn:mozilla:install-manifest">
+    <em:name>${xmlEscape(pkg.xpi.name)}</em:name>
+    <em:description>${xmlEscape(pkg.description)}</em:description>
+    <em:bootstrap>true</em:bootstrap>
+    <em:id>${xmlEscape(pkg.id)}</em:id>
+    <em:version>${xmlEscape(version)}</em:version>
+    <em:homepageURL>${xmlEscape(pkg.homepage)}</em:homepageURL>
+    <em:creator>${xmlEscape(pkg.author.name)}</em:creator>
+    <em:updateURL>${xmlEscape(`${pkg.xpi.releaseURL}update.rdf`)}</em:updateURL>
+    <em:type>2</em:type>
+    <em:targetApplication>
+      <Description>
+        <em:id>zotero@chnm.gmu.edu</em:id>
+        <em:minVersion>${xmlEscape(supported.minVersion)}</em:minVersion>
+        <em:maxVersion>${xmlEscape(supported.maxVersion)}</em:maxVersion>
+      </Description>
+      <Description>
+        <em:id>juris-m@juris-m.github.io</em:id>
+        <em:minVersion>${xmlEscape(supported.minVersion)}</em:minVersion>
+        <em:maxVersion>${xmlEscape(supported.maxVersion)}</em:maxVersion>
+      </Description>
+    </em:targetApplication>
+  </Description>
+</RDF>
+`
+
+  const updateRdf = `<?xml version="1.0" encoding="utf-8" ?>
+<RDF:RDF xmlns:RDF="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:em="http://www.mozilla.org/2004/em-rdf#">
+  <RDF:Description about="urn:mozilla:extension:${xmlEscape(pkg.id)}">
+    <em:updates>
+      <RDF:Seq>
+        <RDF:li>
+          <RDF:Description>
+            <em:version>${xmlEscape(version)}</em:version>
+            <em:targetApplication>
+              <RDF:Description>
+                <em:id>zotero@chnm.gmu.edu</em:id>
+                <em:minVersion>${xmlEscape(supported.minVersion)}</em:minVersion>
+                <em:maxVersion>${xmlEscape(supported.maxVersion)}</em:maxVersion>
+                <em:updateLink>${xmlEscape(pkg.xpi.updateLink.replace('{version}', version))}</em:updateLink>
+              </RDF:Description>
+            </em:targetApplication>
+            <em:targetApplication>
+              <RDF:Description>
+                <em:id>juris-m@juris-m.github.io</em:id>
+                <em:minVersion>${xmlEscape(supported.minVersion)}</em:minVersion>
+                <em:maxVersion>${xmlEscape(supported.maxVersion)}</em:maxVersion>
+                <em:updateLink>${xmlEscape(pkg.xpi.updateLink.replace('{version}', version))}</em:updateLink>
+              </RDF:Description>
+            </em:targetApplication>
+          </RDF:Description>
+        </RDF:li>
+      </RDF:Seq>
+    </em:updates>
+  </RDF:Description>
+</RDF:RDF>
+`
+
+  fs.writeFileSync(path.join(__dirname, 'build', 'install.rdf'), installRdf, 'utf8')
+  fs.writeFileSync(path.join(__dirname, 'gen', 'update.rdf'), updateRdf, 'utf8')
+}
 
 function js(src) {
   return src.replace(/[.]ts$/, '.js')
@@ -59,6 +135,8 @@ async function bundle(config) {
 }
 
 async function build() {
+  generateLegacyMetadata()
+
   await bundle({
     exportGlobals: true,
     entryPoints: [ 'bootstrap.ts' ],
---
+++ gen/version.cjs
@@ -0,0 +1,1 @@
+module.exports = { version: "1.1.15.zhangxing.zhangxing-MacBook-Pro.local" }
